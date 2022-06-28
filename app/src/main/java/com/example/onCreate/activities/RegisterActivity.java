230075bc13aa9b95faeae861b02855b642209b83
Victor Eduardo Goncalves <47225453+Kiopsy@users.noreply.github.com>
2022-06-28 16:41:47 -0700

    Fixing register screen to allow for profile picture select

--- app/src/main/java/com/example/onCreate/activities/RegisterActivity.java
+++ app/src/main/java/com/example/onCreate/activities/RegisterActivity.java
@@ -4,35 +4,63 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.onCreate.R;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String mTAG = "RegisterActivity";
+    private ImageView mIvProfilePicture;
     private EditText mEtUsername;
     private EditText mEtPassword;
     private EditText mEtEmail;
+    private EditText mEtJobDescription;
+    private EditText mEtGeneralDescription;
+    private Button mBtnProfPic;
     private Button mBtnSignup;
+    private Bitmap selectedImage;
+    public final static int mPICK_PHOTO_CODE = 1046;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mIvProfilePicture = findViewById(R.id.ivProfilePicture);
         mEtUsername = findViewById(R.id.etUsername);
         mEtPassword = findViewById(R.id.etPassword);
         mEtEmail = findViewById(R.id.etEmail);
+        mEtJobDescription = findViewById(R.id.etJobDescription);
+        mEtGeneralDescription = findViewById(R.id.etGeneralDescription);
+        mBtnProfPic = findViewById(R.id.btnProfImage);
         mBtnSignup = findViewById(R.id.btnSignup);
+
+        // Set profile picture onClick listener
+        mBtnProfPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPickPhoto(v);
+            }
+        });
 
         // Set signup button onClick Listener
         mBtnSignup.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +70,12 @@ public class RegisterActivity extends AppCompatActivity {
                 String username = mEtUsername.getText().toString();
                 String password = mEtPassword.getText().toString();
                 String email = mEtEmail.getText().toString();
-                signupUser(username, password, email);
+                String jobDescription = mEtJobDescription.getText().toString();
+                String generalDescription = mEtGeneralDescription.getText().toString();
+
+                // TODO: SET CONSTRAINTS FOR INPUT HERE
+                
+                signupUser(username, password, email, jobDescription, generalDescription);
             }
         });
 
@@ -51,7 +84,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     // Create a new Parse
-    private void signupUser(String username, String password, String email) {
+    private void signupUser(String username, String password, String email, String jobDescription, String generalDescription) {
         Log.i(mTAG, "Attempting to create user: " + username);
 
         // Create the ParseUser & set core properties
@@ -59,6 +92,9 @@ public class RegisterActivity extends AppCompatActivity {
         user.setUsername(username);
         user.setPassword(password);
         user.setEmail(email);
+        user.put("jobDescription", jobDescription);
+        user.put("generalDescription", generalDescription);
+        user.put("profileImage", bitmapToParseFile(selectedImage));
 
         // Invoke signUpInBackground
         user.signUpInBackground(new SignUpCallback() {
@@ -73,6 +109,55 @@ public class RegisterActivity extends AppCompatActivity {
                 Toast.makeText(RegisterActivity.this, "success", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, mPICK_PHOTO_CODE);
+    }
+
+    // converts a bitmap to a ParseFile
+    public ParseFile bitmapToParseFile(Bitmap image) {
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG,100,byteArrayOutputStream);
+        byte[] imageByte = byteArrayOutputStream.toByteArray();
+        ParseFile parseFile = new ParseFile("image_file.png",imageByte);
+        return parseFile;
+    }
+
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((data != null) && requestCode == mPICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            selectedImage = loadFromUri(photoUri);
+
+            // Load the selected image into a preview
+            mIvProfilePicture.setImageBitmap(selectedImage);
+        }
     }
 
     // Intent to go to the homepage
